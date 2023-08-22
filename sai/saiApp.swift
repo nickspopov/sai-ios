c4d1f0e177f19954e86725b83c61634237332946
Nick Popov <48173606+nickspopov@users.noreply.github.com>
2023-08-22 11:02:39 +0300

    first commit

--- sai/saiApp.swift
+++ sai/saiApp.swift
@@ -7,8 +7,17 @@
 
 import SwiftUI
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    return true
+  }
+}
+
 @main
 struct saiApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {

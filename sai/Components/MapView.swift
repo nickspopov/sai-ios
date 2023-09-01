ec18be20e35d401008f0a581f492a86e5cdf0baa
Nick Popov <48173606+nickspopov@users.noreply.github.com>
2023-09-01 12:10:57 +0300

    adjust mapView region

--- sai/Components/MapView.swift
+++ sai/Components/MapView.swift
@@ -22,9 +22,7 @@ struct MapView: UIViewRepresentable {
         mapView.setUserTrackingMode(.none, animated: true)
         mapView.delegate = context.coordinator
         
-        mapView.region = MKCoordinateRegion(center: CLLocationCoordinate2D(
-            latitude: 50.3, longitude: 30.4
-        ), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.2))
+        mapView.setRegion(getCoordianteRegion(), animated: true)
         
         setOverlay(mapView)
     
@@ -33,20 +31,48 @@ struct MapView: UIViewRepresentable {
     
     func updateUIView(_ uiView: MKMapView, context: Context) {
         setOverlay(uiView)
+        uiView.setRegion(getCoordianteRegion(), animated: true)
     }
     
-    func setOverlay(_ uiView: MKMapView) {
+    private func getLocationHistoryPolyline() -> MKOverlay {
         let line = MKPolyline(coordinates: locationHistory, count: locationHistory.count)
         
+        return line
+    }
+    
+    private func setOverlay(_ uiView: MKMapView) {
+        uiView.removeOverlays(uiView.overlays)
+        let line = getLocationHistoryPolyline()
+        
         uiView.addOverlay(line)
+    }
+    
+    
+    private func getCoordianteRegion() -> MKCoordinateRegion {
+        if(locationHistory.isEmpty) {
+            return MKCoordinateRegion(
+                center: CLLocationCoordinate2D(latitude: 50.39644, longitude: 30.5488),
+                span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+            )
+        }
+        
+        let polygon = getLocationHistoryPolyline()
+        var boundingMapRect = polygon.boundingMapRect
+        
+        boundingMapRect.size = MKMapSize(width: boundingMapRect.size.width * 1.4, height: boundingMapRect.size.height * 1.4)
+        
+        var region = MKCoordinateRegion(boundingMapRect)
+        region.center.longitude -= region.span.longitudeDelta / 3.75
+        
+        return region
+    
     }
     
     class MapViewCoordinator: NSObject, MKMapViewDelegate {
         func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
             let renderer = MKPolylineRenderer(overlay: overlay)
-            renderer.strokeColor = .red
-            renderer.fillColor = .black
-            renderer.lineWidth = 5
+            renderer.strokeColor = .white
+            renderer.lineWidth = 2
             return renderer
         }
     }
@@ -57,7 +83,7 @@ struct MapView: UIViewRepresentable {
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        let locationHistory = [
+        let locationHistory: [CLLocationCoordinate2D] = [
             CLLocationCoordinate2D(latitude: 50.39644, longitude: 30.5488),
             CLLocationCoordinate2D(latitude: 50.38644, longitude: 30.4488)
         ]

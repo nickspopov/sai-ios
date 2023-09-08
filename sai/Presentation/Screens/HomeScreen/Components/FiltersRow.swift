3e0a95b9d7b0aa3168176e1313d69709b0a73343
Nick Popov <48173606+nickspopov@users.noreply.github.com>
2023-09-08 18:49:36 +0300

    add animation for switch tabs

--- sai/Presentation/Screens/HomeScreen/Components/FiltersRow.swift
+++ sai/Presentation/Screens/HomeScreen/Components/FiltersRow.swift
@@ -10,35 +10,49 @@ import SwiftUI
 struct FiltersRow: View {
     
     @Binding var activeIndex: Int
+    @State var activeIndexAnimated: Int = 0
+    
+    @Namespace var namesapce
     
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false) {
             HStack(spacing: 10) {
-                filterItem(name: "All", isActive: activeIndex == 0, onPress: {activeIndex = 0})
-                filterItem(name: "Tasks", isActive: activeIndex == 1, onPress: {activeIndex = 1})
-                filterItem(name: "Activity", isActive: activeIndex == 2, onPress: {activeIndex = 2})
-                filterItem(name: "Meals", isActive: activeIndex == 3, onPress: {activeIndex = 3})
+                filterItem(name: "All", isActive: activeIndexAnimated == 0, onPress: {activeIndex = 0})
+                filterItem(name: "Tasks", isActive: activeIndexAnimated == 1, onPress: {activeIndex = 1})
+                filterItem(name: "Activity", isActive: activeIndexAnimated == 2, onPress: {activeIndex = 2})
+                filterItem(name: "Meals", isActive: activeIndexAnimated == 3, onPress: {activeIndex = 3})
             }
             .padding(.leading, 16)
+            .onChange(of: activeIndex) { newValue in
+                withAnimation {
+                    activeIndexAnimated = newValue
+                }
+            }
         }
     }
     
     func filterItem(name:String, isActive: Bool, onPress: @escaping () -> Void) -> some View {
-        return HStack(alignment: .center, spacing: 16) {
+        return Button(action: {onPress()}) {
             Typography(name, .medium(.five))
                 .foregroundColor(isActive ? .black : .white)
         }
         .padding(.horizontal, 20)
         .padding(.vertical, 12)
-        .background(isActive ? .white : .clear)
+        .background(
+            ZStack {
+                if isActive {
+                    Color.white
+                        .cornerRadius(200)
+                        .matchedGeometryEffect(id: "FiltersRow_bg_active_index", in: namesapce)
+                }
+            }
+        )
         .overlay(content: {
-            RoundedRectangle(cornerRadius: 200)
-                .stroke(Color.white, lineWidth: 1)
+            RoundedRectangle(cornerRadius: .infinity)
+                .inset(by: 0.5)
+                .stroke(Color(red: 0.82, green: 0.82, blue: 0.82), lineWidth: 1)
         })
-        .cornerRadius(200)
-        .onTapGesture {
-            onPress()
-        }
+        .buttonStyle(PlainButtonStyle())
     }
 }
 

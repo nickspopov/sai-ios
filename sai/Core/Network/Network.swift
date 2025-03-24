9f740c79f0040d3005d1df55c063f11e1310824e
Nick Popov <48173606+nickspopov@users.noreply.github.com>
2025-03-24 15:38:59 +0100

    use remote fastAPI

--- sai/Core/Network/Network.swift
+++ sai/Core/Network/Network.swift
@@ -16,7 +16,7 @@ class Network {
         let cache = InMemoryNormalizedCache()
         let store = ApolloStore(cache: cache)
         let provider = NetworkInterceptorProvider(client: client, store: store)
-        let url = URL(string: "https://fastapi-sai.koyeb.app/graphql")!
+        let url = URL(string: "http://localhost:8000/graphql")!
         let transport = RequestChainNetworkTransport(interceptorProvider: provider, endpointURL: url)
 
         return ApolloClient(networkTransport: transport, store: store)
